e55342d8471c5f7ad3ccb872bf360f7543c638a3
trananhtuuuuuuuu <trananhtu01012004@gmail.com>
2025-10-16 13:39:42 +0700

    set up for project

---
+++ Spring_Security_Project/src/main/java/org/example/spring_security_project/SpringSecurityProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.spring_security_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityProjectApplication.class, args);
+    }
+
+}
